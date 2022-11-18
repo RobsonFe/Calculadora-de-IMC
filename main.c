f7d53aec104f562c116e2837d5ab683d0f936608
Robson Ferreira <115955245+RobsonFe@users.noreply.github.com>
2022-11-18 08:47:12 -0300

    Indentação
    Atualizei a indentação do código para ficar algo mais limpo e organizado.

--- main.c
+++ main.c
@@ -9,43 +9,49 @@
 int main()
 {
 
-double peso, altura, IMC ;
+    double peso, altura, IMC ;
 
-printf("\t\tCalcule o seu IMC\n\n");
+    printf("\t\tCalcule o seu IMC\n\n");
 
-printf("Insira o seu peso: ");
-scanf("%lf", &peso);
+    printf("Insira o seu peso: ");
+    scanf("%lf", &peso);
 
-printf("Insira a sua altura: ");
-scanf("%lf", &altura);
+    printf("Insira a sua altura: ");
+    scanf("%lf", &altura);
 
-IMC = peso/(altura*altura);
+    IMC = peso/(altura*altura);
 
 
-printf("O resultado do seu IMC: %.2lf", IMC );
+    printf("O resultado do seu IMC: %.2lf", IMC );
 
-if (IMC < 18.5) {
+    if (IMC < 18.5)
+    {
 
-    printf("\nO seu IMC = Magreza\n");
-}
-else if (IMC > 18.5 && IMC < 24.8) {
+        printf("\nO seu IMC = Magreza\n");
+    }
+    else if (IMC > 18.5 && IMC < 24.8)
+    {
 
-    printf("\nO seu IMC = Normal\n");
-}
-else if (IMC > 25 && IMC < 29.9){
+        printf("\nO seu IMC = Normal\n");
+    }
+    else if (IMC > 25 && IMC < 29.9)
+    {
 
-    printf("\nO seu IMC = Sobrepeso\n");
-}
-else if (IMC > 30 && IMC < 34.9){
+        printf("\nO seu IMC = Sobrepeso\n");
+    }
+    else if (IMC > 30 && IMC < 34.9)
+    {
 
-    printf("\nO seu IMC = Obesidade Grau 1\n");
-}
-else if (IMC > 35 && IMC <39.9){
-    printf("\nO seu IMC = Obesidade grau 2\n");
-}
-else if (IMC > 40){
+        printf("\nO seu IMC = Obesidade Grau 1\n");
+    }
+    else if (IMC > 35 && IMC <39.9)
+    {
+        printf("\nO seu IMC = Obesidade grau 2\n");
+    }
+    else if (IMC > 40)
+    {
 
-    printf("\nO seu IMC = Obesidade Grau 3");
-}
+        printf("\nO seu IMC = Obesidade Grau 3");
+    }
     return 0;
 }
