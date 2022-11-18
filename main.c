4ac886c84e879296c8a67142458979b49058306c
Robson Ferreira <115955245+RobsonFe@users.noreply.github.com>
2022-11-18 08:07:14 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-#include <math.h>
+
+// Projeto Feito por Robson Ferreira
+
+// Aluno: Robson Ferreira;
+// Curso: Analise e Desenvolvimento de Sistemas;
+// 1º Periodo;
 
 int main()
 {
@@ -21,26 +26,26 @@ printf("O resultado do seu IMC: %.2lf", IMC );
 
 if (IMC < 18.5) {
 
-    printf("\nO seu IMC e Magreza\n");
+    printf("\nO seu IMC = Magreza\n");
 }
 else if (IMC > 18.5 && IMC < 24.8) {
 
-    printf("\nO seu IMC e Normal\n");
+    printf("\nO seu IMC = Normal\n");
 }
 else if (IMC > 25 && IMC < 29.9){
 
-    printf("\nO seu IMC e Sobrepeso\n");
+    printf("\nO seu IMC = Sobrepeso\n");
 }
 else if (IMC > 30 && IMC < 34.9){
 
-    printf("\nO seu IMC e Obesidade Grau 1\n");
+    printf("\nO seu IMC = Obesidade Grau 1\n");
 }
 else if (IMC > 35 && IMC <39.9){
-    printf("\nO seu IMC e Obesidade grau 2\n");
+    printf("\nO seu IMC = Obesidade grau 2\n");
 }
 else if (IMC > 40){
 
-    printf("\nO seu IMC e Obesidade Grau 3");
+    printf("\nO seu IMC = Obesidade Grau 3");
 }
     return 0;
 }
